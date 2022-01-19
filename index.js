fa02df10a488e379617244bb30bcc02c38940a31
Cecilia Daninthe <cecilia.daninthe@gmail.com>
2022-01-18 19:33:04 -0500

    commit tree 6793980fa94cd6cb5d667718a9b37b59f1d6dbaa

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
 function addingEventListener() {
     const input = document.getElementById('input');
     function clickAlert() {
-    alert('I was clicked!');
-        }
+        alert('I was clicked!');
+    }
     input.addEventListener('click', clickAlert);
 };
 
 addingEventListener();
+
+
+
+
+
+
+
+
+
+
 
